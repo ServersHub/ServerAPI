539bfe4093994081fc435feb5dee790b85487097
Pelayori <31128562+Pelayori@users.noreply.github.com>
2023-10-30 17:26:43 +0100

    Fix bug with log folder, changed init to exported func, fix potential bug on caching

--- AsaApi/main.cpp
+++ AsaApi/main.cpp
@@ -2,13 +2,25 @@
 #include "..\Private\IBaseApi.h"
 #include "..\Private\Ark\ArkBaseApi.h"
 
+#include "Tools.h"
+#include <filesystem>
+
 void Init()
 {
+	namespace fs = std::filesystem;
+
 	//Temporary fix for console output
 	AllocConsole();
 	FILE* p_cout;
 	freopen_s(&p_cout, "conout$", "w", stdout);
 	SetConsoleOutputCP(CP_UTF8);
+
+	const std::string current_dir = API::Tools::GetCurrentDir();
+
+	if (!fs::exists(current_dir + "/logs"))
+	{
+		fs::create_directory(current_dir + "/logs");
+	}
 
 	Log::Get().Init("API");
 
@@ -16,11 +28,16 @@ void Init()
 	API::game_api->Init();
 }
 
-void Unload()
+extern "C" ARK_API void InitApi()
 {
+	Init();
 }
 
-extern "C" ARK_API void ExportFunc() {}
+void Unload()
+{
+
+}
+
 
 BOOL APIENTRY DllMain(HMODULE hModule,
 	DWORD  ul_reason_for_call,
@@ -30,7 +47,6 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-		Init();
 		break;
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
