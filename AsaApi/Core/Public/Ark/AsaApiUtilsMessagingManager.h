913a640e1990f994dc2320fb287fbf5d5f88fc07
Pelayori <31128562+Pelayori@users.noreply.github.com>
2023-12-29 21:45:59 +0100

    Added some more messages function to the asa api utils mod messaging manager

--- AsaApi/Core/Public/Ark/AsaApiUtilsMessagingManager.h
+++ AsaApi/Core/Public/Ark/AsaApiUtilsMessagingManager.h
@@ -14,16 +14,55 @@ public:
 		return {};
 	}
 
-protected:
-	void SendServerMessage_Impl(AShooterPlayerController* player_controller, FLinearColor msg_color, const FString& msg) override
+	// this function lets you send a notification to a player using the pretty widgets from the api utils mod, will all the possible params, to specific player
+	FORCEINLINE FString SendNotificationPrettyToPlayer(APlayerController* PC, const FString& Text, const FLinearColor& BackgroundColor, const FLinearColor& TextColor,
+		const double TextScale, const double Duration, const Position TextJustification, const Position ScreenPosition, const bool bAddToChat)
 	{
-		AsaApiModUtils::AddNotification(msg, { player_controller->GetEOSId() }, FLinearColor(0,0,0,0), msg_color, 1.0, 0.0, Position::Center, Position::Center, true);
+		TArray<FString> ids;
+		ids.Add(PC->GetEOSId());
+		return AsaApiModUtils::AddNotification(Text, ids, BackgroundColor, TextColor, TextScale, Duration, TextJustification, ScreenPosition, bAddToChat);
 	}
 
+	// this function lets you send a notification to a player using the pretty widgets from the api utils mod, will all the possible params, to specific player
+	FORCEINLINE FString SendNotificationPrettyToPlayer(const FString& EOSid, const FString& Text, const FLinearColor& BackgroundColor, const FLinearColor& TextColor,
+		const double TextScale, const double Duration, const Position TextJustification, const Position ScreenPosition, const bool bAddToChat)
+	{
+		TArray<FString> ids;
+		ids.Add(EOSid);
+		return AsaApiModUtils::AddNotification(Text, ids, BackgroundColor, TextColor, TextScale, Duration, TextJustification, ScreenPosition, bAddToChat);
+	}
+
+	// this function lets you send a notification to a player using the pretty widgets from the api utils mod, will all the possible params, to all players
+	FORCEINLINE FString SendNotificationPrettyToPlayers(TArray<APlayerController*> PCs, const FString& Text, const FLinearColor& BackgroundColor, const FLinearColor& TextColor,
+		const double TextScale, const double Duration, const Position TextJustification, const Position ScreenPosition, const bool bAddToChat)
+	{
+		TArray<FString> ids;
+		for (APlayerController* PC : PCs)
+			ids.Add(PC->GetEOSId());
+		return AsaApiModUtils::AddNotification(Text, ids, BackgroundColor, TextColor, TextScale, Duration, TextJustification, ScreenPosition, bAddToChat);
+	}
+
+	FORCEINLINE FString SendNotificationPrettyToPlayers(TArray<FString> IDs, const FString& Text, const FLinearColor& BackgroundColor, const FLinearColor& TextColor,
+		const double TextScale, const double Duration, const Position TextJustification, const Position ScreenPosition, const bool bAddToChat)
+	{
+		return AsaApiModUtils::AddNotification(Text, IDs, BackgroundColor, TextColor, TextScale, Duration, TextJustification, ScreenPosition, bAddToChat);
+	}
+protected:
+	// changes the server messages to use pretty widgets from the api utils mod
+	void SendServerMessage_Impl(AShooterPlayerController* player_controller, FLinearColor msg_color, const FString& msg) override
+	{
+		TArray<FString> ids;
+		ids.Add(player_controller->GetEOSId());
+		AsaApiModUtils::AddNotification(msg, ids, FLinearColor(0,0,0,0), msg_color, 1.0, 0.0, Position::Center, Position::Center, true);
+	}
+
+	// changes notifications to use pretty widgets from the api utils mod, this version mostly mimics old ASE notifications
 	void SendNotification_Impl(AShooterPlayerController* player_controller, FLinearColor color, float display_scale,
 		float display_time, UTexture2D* icon, const FString& msg) override
 	{
-		AsaApiModUtils::AddNotification(msg, { player_controller->GetEOSId() }, FLinearColor(0, 0, 0, 0), color, display_scale, display_time, Position::Center, Position::Center, false);
+		TArray<FString> ids;
+		ids.Add(player_controller->GetEOSId());
+		AsaApiModUtils::AddNotification(msg, ids, FLinearColor(0, 0, 0, 0), color, display_scale, display_time, Position::Center, Position::Center, false);
 	}
 };
 
