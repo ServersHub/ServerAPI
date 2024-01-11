ba79bf08fb40ada0a0cba55634986e7ef27c208d
Pelayori <31128562+Pelayori@users.noreply.github.com>
2024-01-11 18:09:26 +0100

    Added another constructor for specifying notification id in special cases

--- AsaApi/Core/Public/API/ARK/ModParamStructs.h
+++ AsaApi/Core/Public/API/ARK/ModParamStructs.h
@@ -129,6 +129,22 @@ struct AsaApiUtilsNotification
 		, bAddToChat(bAddToChat)
 	{
 	}
+
+	AsaApiUtilsNotification(
+		const FString& Notificationid,
+		const FString& Text,
+		const TArray<FString>& RecipientEOS,
+		const FLinearColor& BackgroundColor,
+		const FLinearColor& TextColor,
+		const double& DisplayScale,
+		const double& DisplayTime,
+		const Position& TextJustification,
+		const Position& NotificationScreenPosition,
+		const bool& bAddToChat
+	) : AsaApiUtilsNotification(Text, RecipientEOS, BackgroundColor, TextColor, DisplayScale, DisplayTime, TextJustification, NotificationScreenPosition, bAddToChat)
+	{
+		NotificationId = Notificationid;
+	}
 };
 
 struct AddNotification_Params
